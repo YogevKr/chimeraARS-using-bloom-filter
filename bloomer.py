86e3e1471a6c79c60907a0ee4acaf5e4d46a5923
Yogev Kriger <yogev@appcard.com>
2020-03-27 21:25:31 +0300

    multi process

--- bloomer.py
+++ bloomer.py
@@ -25,26 +25,35 @@ def num_of_bits(ideal_num_elements_n, error_rate):
     return numerator / denominator
 
 
-def chimera_ars_score(host_genome, target_genome, max_k=50, error_rate=0.0001):
-    total_hits_list = []
+def get_populted_bloom_filter(k, host_genome, error_rate):
+    max_elements = 4**k if 4**k < len(host_genome) else len(host_genome)
 
-    for k in range(1,max_k+1):
-        max_elements = 4**k if 4**k < len(host_genome) else len(host_genome)
+    bloom = BloomFilter(max_elements=max_elements, error_rate=error_rate)
+    print(f"k={k}, max_elements={max_elements}, num_bits_m={bloom.num_bits_m}")
 
-        bloom = BloomFilter(max_elements=max_elements, error_rate=error_rate)
-        print(f"k={k}, max_elements={max_elements}, num_bits_m={bloom.num_bits_m}")
+    for s in window(host_genome, n=k):
+        bloom.add("".join(s))
 
-        for s in window(host_genome, n=k):
-            bloom.add("".join(s))
+    return bloom
 
-        k_hits= sum(map(lambda x: x in bloom, window(target_genome, n=k)))
 
-        total_hits_list.append(k_hits)
-        print(f"k_hits={k_hits}")
+def count_hits(k_bloom_tuple, target_genome):
+    k, bloom = k_bloom_tuple
+    return sum(map(lambda x: x in bloom, window(target_genome, n=k)))
 
-        if k_hits <= 1: break
+
+def chimera_ars_score(host_genome, target_genome, max_k=16, error_rate=0.0001, num_processors=0):
+    from multiprocessing import Pool, cpu_count
+    import functools
+
+    pool = Pool(num_processors or cpu_count())
+
+    populate_runner = functools.partial(get_populted_bloom_filter, host_genome=host_genome, error_rate=error_rate)
+    bloom_models = pool.map(populate_runner, range(1, max_k+1))
+
+    hit_runner = functools.partial(count_hits, target_genome=target_genome)
+    total_hits_list = pool.map(hit_runner, ((k, bloom) for k, bloom in enumerate(bloom_models, start=1)))
 
     print(total_hits_list)
     
     return sum(total_hits_list) / len(target_genome)
-
